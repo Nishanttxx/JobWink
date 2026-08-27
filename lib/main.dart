59dd87f330c3f0c10a42735350c3c7a6bdbe2254
Nishant Arya <na6236786@gmail.com>
2026-08-27 23:51:29 +0530

    chore: remove unused supabase_flutter import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'animations/lenis_scroll.dart';
 import 'animations/page_transitions.dart';
 import 'providers/auth_provider.dart';
